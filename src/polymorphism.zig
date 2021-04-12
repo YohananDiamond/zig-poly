a45b5e8d242f30e6b625ba916934eccae2e3d830
YohananDiamond <mitonanan12@gmail.com>
2021-04-11 21:19:13 -0300

    Initial commit!

---
+++ src/polymorphism.zig
@@ -0,0 +1,73 @@
+const std = @import("std");
+
+test "entry point" {
+    _ = @import("tests.zig");
+}
+
+pub const SelfType = opaque {};
+
+pub fn Interface(comptime VTableT: type) type {
+    return struct {
+        pub fn Impl(comptime Target: anytype) type {
+            return if (@typeInfo(@TypeOf(Target)) == .EnumLiteral and Target == .Dyn) struct {
+                vtable_ptr: *const VTableT,
+                object_ptr: *SelfType,
+
+                pub fn init(ptr: anytype) @This() {
+                    const ChildType = PointerChildType(@TypeOf(ptr));
+                    expectIsMutablePointer(@TypeOf(ptr));
+
+                    comptime validateVtable(ChildType);
+                    const vtable_ptr = comptime getTypeVTable(ChildType);
+
+                    return .{
+                        .vtable_ptr = vtable_ptr,
+                        .object_ptr = @ptrCast(*SelfType, ptr),
+                    };
+                }
+            } else if (@TypeOf(Target) == type) struct {
+                object_ptr: *Target,
+
+                // comptime {
+                //     validateVtable(Target);
+                // }
+
+                pub const vtable_ptr: *const VTableT = comptime getTypeVTable(Target);
+
+                pub fn init(ptr: *Target) @This() {
+                    return .{ .object_ptr = ptr };
+                }
+            } else {
+                @compileError("Invalid impl target (expected either a type or .Dyn, found " ++ @typeName(@TypeOf(Target)) ++ ")");
+            };
+        }
+
+        pub fn staticInit(ptr: anytype) Impl(PointerChildType(@TypeOf(ptr))) {
+            const ChildType = PointerChildType(@TypeOf(ptr));
+            expectIsMutablePointer(@TypeOf(ptr));
+
+            return Impl(ChildType).init(ptr);
+        }
+
+        pub const dynamicInit = Impl(.Dyn).init;
+
+        // TODO: pub fn autoStaticInit() or something
+    };
+}
+
+fn PointerChildType(comptime PointerType: type) type {
+    return switch (@typeInfo(PointerType)) {
+        .Pointer => |info| info.child,
+        else => @compileError("Expected pointer, found " ++ @typeName(PointerType)),
+    };
+}
+
+fn expectIsMutablePointer(comptime PointerType: type) void {
+    if (@typeInfo(PointerType).Pointer.is_const) {
+        @compileError("Expected mutable pointer, found const pointer");
+    }
+}
+
+fn validateVtable(comptime VTableT: type) void {
+    unreachable;
+}
---
+++ src/tests.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const testing = std.testing;
+
+const poly = @import("polymorphism.zig");
+
+const MagicNumber = poly.Interface(struct {
+    getMagicNumber: fn (self: *poly.SelfType) u8,
+});
+
+const Five = struct {
+    const Self = @This();
+
+    pub fn getMagicNumber(self: *Self) u8 {
+        return 5;
+    }
+};
+
+test "static dispatching" {
+    var five_n = Five{};
+    const magic_num = MagicNumber.staticInit(&five_n);
+
+    // testing.expect(magic_num.call("getMagicNumber", .{}) == 5);
+}
